32aaf10ad6836add539b512bd02ccf0c1374bb4c
Yi Sun <syqust@gmail.com>
2020-05-27 08:44:34 +0800

    Build kernel with empty localversion
    Signed-off-by: Yi Sun <syqust@gmail.com>

--- build_kernel_qemu/install_kernel_local.sh
+++ build_kernel_qemu/install_kernel_local.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-make && make modules && make modules_install
+make LOCALVERSION="" -j4
+make modules && make modules_install
 cd /usr/lib/modules/5.7.0-rc5/
 
 # Remove useless symbols, reduce initrd a lot! Or kernel will not boot up.
